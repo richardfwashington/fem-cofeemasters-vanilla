496859b2e06a7ae7061dcaae5fd1fe31c2b5c02f
Richard Washington <richard.washington@taxassist.co.uk>
2023-11-30 16:37:06 +0000

    :sparkles: Add and import API and Store modules

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+import Store from "./services/Store.js";
+import API from "./services/API.js";
+
+window.app = {};
+app.store = Store;
+
 window.addEventListener("DOMContentLoaded", () => {
   let nav = $("nav");
 
