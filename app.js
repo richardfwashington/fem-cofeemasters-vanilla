3718dc6f5a03acf4b076fa21ea68f73b4bbd347d
Richard Washington <richard.washington@taxassist.co.uk>
2023-11-30 16:24:52 +0000

    :sparkles:  Add various aliases to reduce code verbosity

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
-  let nav = document.querySelector("nav");
+  let nav = $("nav");
 
   console.log(nav);
 
@@ -9,4 +9,31 @@ window.addEventListener("DOMContentLoaded", () => {
   `;
 });
 
-window.addEventListener("DOMContentLoaded", () => {});
+// Use $ instead of document.querySelector - just like JQuery
+const $ = function (args) {
+  return document.querySelector(args);
+};
+
+// Use $$ instead of document.querySelectorAll
+const $$ = function (args) {
+  return document.querySelectorAll(args);
+};
+
+// Use .on instead of addEventListener
+HTMLElement.prototype.on = function (a, b, c) {
+  return this.addEventListener(a, b, c);
+};
+
+// Use .off instead of removeEventListener
+HTMLElement.prototype.off = function (a, b) {
+  return this.removeEventListener(a, b);
+};
+
+// Use element.$ instead of element.querySelector
+HTMLElement.prototype.$ = function (s) {
+  return this.querySelector(s);
+};
+// Use element.$$ instead of element.querySelectorAll
+HTMLElement.prototype.$$ = function (s) {
+  return this.querySelectorAll(s);
+};
