62763ac3a4eb3103a9c39327acf5d8086fbabc48
Richard Washington <richard.washington@taxassist.co.uk>
2023-12-02 13:37:16 +0000

    :ambulance: Add router

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import Store from "./services/Store.js";
 import API from "./services/API.js";
 import { loadData } from "./services/Menu.js";
+import Router from "./services/Router.js";
 
 window.app = {};
 app.store = Store;
+app.router = Router;
 
 // Wait for everything to be in memory before we try and manipulate the DOM
 window.addEventListener("DOMContentLoaded", () => {
---
+++ services/Router.js
@@ -0,0 +1,8 @@
+const Router = {
+  init: () => {},
+  go: (route, addToHistory = true) => {
+    console.log(`Going to ${route}`);
+  },
+};
+
+export default Router;
